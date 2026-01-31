c539cd364d16e037bd13d5da119127a475fddb51
Brijes Mandal <brijes.mandal01@gmail.com>
2026-01-31 15:41:04 -0600

    Models Setup

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT;
+const cors = require("cors");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 8000;
 
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(cookieParser());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(console.log("Database Connected"))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => console.log("Server running on port:", PORT));
